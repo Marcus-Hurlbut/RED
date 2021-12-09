f0eb078cd208611a08c939428c8284a5457cb271
Thugs4Less <Thugs4Less@DESKTOP-QST9L7O>
2021-12-09 12:10:00 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+
+#include <windows.h>
+#include "Renderer.h"
+
+#define WIDTH 400
+#define HEIGHT 400
+
+#undef main
+
+
+int main()
+{
+    Renderer vulkan;
+    Sleep(5000);
+
+    return 0;
+}
+
+// mingw32-make -f Makefile
+
+
